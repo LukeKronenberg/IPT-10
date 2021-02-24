03aa9771659e2f19e0ff8b0c6233449be4f7874c
Virus7021 <kronenbergluke@gmail.com>
2021-02-24 16:25:31 +0100

    Ready for code push

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import './App.css';
+
+//funtions here
+
+function App() {
+
+  //variables here
+  //how to bind --> const [val, updateVal] = useState(1)
+  
+  //funtions here
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* HTML here */}
+      </header>
+    </div>
+  );
+}
+
+
+export default App;
